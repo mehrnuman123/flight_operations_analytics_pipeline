c42f5213f142f26aba46ab8094d433002a89a9c3
mnuman2204 <mnuman2024@gmail.com>
2026-07-14 15:20:56 +0300

    code refectoring

--- source_data.py
+++ source_data.py
@@ -3,5 +3,5 @@ import kagglehub
 # Download latest version
 #path = kagglehub.dataset_download("ioanagheorghiu/historical-flight-and-weather-data")
 
-dataset_path="/home/numan/.cache/kagglehub/datasets/ioanagheorghiu/historical-flight-and-weather-data/versions/1"
+
 
